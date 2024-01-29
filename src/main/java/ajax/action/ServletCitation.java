e3149ca76f850efc1a7a596a8c93a5a2baa44c51
Manuvai <44281637+manuvai@users.noreply.github.com>
2024-01-29 15:12:39 +0100

    first commit

---
+++ src/main/java/ajax/action/ServletCitation.java
@@ -0,0 +1,58 @@
+package ajax.action;
+
+import ajax.bd.Bd;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Interroge la base de données et retourne la liste des citations
+ * sous format XML.
+ */
+@WebServlet(value="/ServletCitation")
+public class ServletCitation extends HttpServlet
+{
+	private static final long serialVersionUID = 5558284359009356382L;
+
+	@Override
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+		{
+		/*----- Type de la réponse -----*/
+		response.setContentType("application/xml;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		try (PrintWriter out = response.getWriter())
+			{
+			/*----- Ecriture de la page XML -----*/
+			out.println("<?xml version=\"1.0\"?>");
+			out.println("<liste_citation>");
+
+			/*----- Récupération des paramètres -----*/
+			String nom = request.getParameter("nomauteur");
+
+			try {
+				/*----- Lecture de liste de mots dans la BD -----*/
+				ArrayList<String> lCitations = Bd.lireCitations(nom);
+
+				for (String citation : lCitations)
+					out.println("<citation>" + citation + "</citation>");
+				}
+			catch (ClassNotFoundException | SQLException ex)
+				{
+				out.println("<citation>Erreur - " + ex.getMessage() + "</citation>");
+				}
+
+			out.println("</liste_citation>");
+			}
+		}
+
+	@Override
+	protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { doGet(request, response); }
+
+} /*----- Fin de la servlet ServletCitation -----*/
---
+++ src/main/java/ajax/action/ServletEncode.java
@@ -0,0 +1,42 @@
+package ajax.action;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Cette servlet récupère un flux et le décode.
+ */
+@WebServlet(value="/ServletEncode")
+public class ServletEncode extends HttpServlet
+{
+	private static final long serialVersionUID = 2561098435378867777L;
+
+	@Override
+	protected void doGet (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+		{
+		/*----- Lecture de la requête en UTF-8 -----*/
+		request.setCharacterEncoding("UTF-8");
+
+		/*----- Type de la réponse -----*/
+		response.setContentType("application/xml;charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		try (PrintWriter out = response.getWriter())
+			{
+			String ch = request.getParameter("texte");
+
+			/*----- Ecriture de la page xml -----*/
+			out.println("<?xml version=\"1.0\"?>");
+			out.println("<msg><![CDATA[" + ch + "]]></msg>");
+			}
+		}
+
+	@Override
+	protected void doPost (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException { doGet(request, response); }
+
+} /*----- Fin de la servlet ServletEncode -----*/
